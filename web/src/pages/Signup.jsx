a168a2a2a83d7ebf3a1f005225dc0336e3d85c8c
ali <maliakbar2147@gmail.com>
2023-09-18 12:37:55 +0500

    drone testing

--- web/src/pages/Signup.jsx
+++ web/src/pages/Signup.jsx
@@ -59,7 +59,7 @@ class Signup extends Component {
 
 
 
-        fetch('http://34.131.242.5:4000/auth/signup', {
+        fetch('http://34.131.77.105:4000/auth/signup', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
